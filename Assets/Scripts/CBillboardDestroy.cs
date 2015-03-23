a54d2108c696117fbcca3a6c9a91867f570f3b9e
crash0verride24 <struka2009@gmail.com>
2015-03-23 09:22:46 -0500

    Billboard pooling
    Added prefab for billboard, and script

---
+++ Assets/Scripts/CBillboardDestroy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CBillboardDestroy : MonoBehaviour 
+{
+    //void OnEnable()
+    //{
+    //    Invoke("Destroy", 2f);
+    //}
+
+    //void Destroy()
+    //{
+    //    gameObject.SetActive(false);
+    //}
+
+    //void OnDisable()
+    //{
+    //    CancelInvoke();
+    //}
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+
+        if(transform.position.z < player.transform.position.z - 30)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
