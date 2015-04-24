9330dd814c8e1b59a2fdab11e9d861fcd1f39a4f
crash0verride24 <struka2009@gmail.com>
2015-04-24 16:18:54 -0500

    Billboard pool script update 1.3(cont.)

--- Assets/Scripts/CBillboardSpawn.cs
+++ Assets/Scripts/CBillboardSpawn.cs
@@ -22,12 +22,12 @@ public class CBillboardSpawn : MonoBehaviour
 
     IEnumerator SpawnLane()
     {
-        while(cam.transform.position.z > -40f)
+        while (cam.transform.position.z > -40f)
         {
-            this.GetComponent<CBillboardPool>().Activate(Random.Range(0, 5), new Vector3(0f, -0.5f, nextPrefabZ * 50f), Quaternion.identity);
+            this.GetComponent<CBillboardPool>().Activate(Random.Range(0, this.GetComponent<CBillboardPool>().objectsToPool.Length), new Vector3(0f, -0.5f, nextPrefabZ * 50f), Quaternion.identity);
             nextPrefabZ++;
 
-            yield return new WaitForSeconds(1.7f);
+            yield return new WaitForSeconds(2.5f);
         }
     }
 }
